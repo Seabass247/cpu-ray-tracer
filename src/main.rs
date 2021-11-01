9f822d248f1664ab261d6c4050efb9dc2b90e975
Seabass247 <sebjfk@gmail.com>
2021-11-01 15:20:46 -0400

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,32 @@
-
-use std::io::Write;
-
-type Color = (u8, u8, u8);
+use image::{ImageBuffer, RgbImage};
 
 fn main() {
-    let imgx = 256;
-    let imgy = 256;
+    const IMAGE_WIDTH: u32 = 256;
+    const IMAGE_HEIGHT: u32 = 256;
 
-    let mut imgbuf: image::RgbImage = image::ImageBuffer::new(imgx, imgy);
-    
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let y = (imgy - 1) - y;
-        let r = x as f32 / imgx as f32;
-        let g = y as f32 / imgy as f32;
-        let b = 0.25;
+    let mut pixel_buf: Vec<u8> = Vec::new();
 
-        let r = (r * 255.999) as u8;
-        let g = (g * 255.999) as u8;
-        let b = (b * 255.999) as u8;
-        
-        *pixel = image::Rgb([r, g, b]);
+    for j in (0..IMAGE_HEIGHT).rev() {
+        println!("\rScanlines remaining: {}", j);
+        for i in 0..IMAGE_WIDTH {
+            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let b = 0.25;
+
+            let ir = (255.99 * r) as u8;
+            let ig = (255.99 * g) as u8;
+            let ib = (255.99 * b) as u8;
+
+            let mut pixel = vec![ir, ig, ib];
+            pixel_buf.append(&mut pixel);
+        }
     }
 
-    
+    let buffer: RgbImage = ImageBuffer::from_raw(IMAGE_WIDTH, IMAGE_HEIGHT, pixel_buf)
+        .expect("Could not create image from buffer");
+    buffer
+        .save("output.png")
+        .expect("Could not save image to file");
 
-    imgbuf.save("output.png").expect("Couldn't save image buffer");
-
-    println!("Image finished." );
+    println!("Done.")
 }
